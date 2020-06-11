27a5624d2d14b50e4ae4897e02508db37cf7ecd8
Olga <aboutrocky94@gmail.com>
2020-06-11 14:40:48 +0300

    Геометрическая прогрессия

--- src/lesson04/Progress.java
+++ src/lesson04/Progress.java
@@ -2,13 +2,21 @@ package lesson04;
 
 import java.util.Scanner;
 
+/**
+ *  Программа, которая выводит арифметическую или геометрическую прогрессию для N чисел
+ */
+
 public class Progress {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a1, a2, quantity, d, an;
+        double bn;
+        String progressType;
 
+        System.out.print("Введите А для арифметической прогрессии, любой символ - для геометрической");
+        progressType = scanner.next();
 
-
+        //ввод данных
         System.out.print("Введите число а1: ");
         a1 = scanner.nextInt();
         System.out.print("Введите число а2: ");
@@ -16,16 +24,28 @@ public class Progress {
         System.out.print("Введите количество чисел в прогрессии: ");
         quantity = scanner.nextInt();
 
+        if (progressType == "А") {
+            System.out.println("Арифметическая прогрессия");
+            System.out.print(a1 + " " + a2 + " ");
 
+            int i = 2;
+            d = a2 - a1;
+            while (i != quantity) {
+                ++i;
+                an = a1 + d * (i - 1);
+                System.out.print(an + " ");
+            }
+        } else {
+            System.out.println("Геометрическая прогрессия");
+            System.out.print(a1 + " " + a2 + " ");
 
-        /*d = a2 - a1;
-        System.out.print(a1 + " " + a2 + " ");
-
-        int i = 2;
-        while (i != quantity) {
-            ++i;
-            an = a1 + d * (i - 1);
-            System.out.print(an + " ");
-        }*/
+            int i = 2;
+            d = a2 / a1;
+            while (i != quantity) {
+                ++i;
+                bn = a1 * Math.pow(d, i - 1);
+                System.out.print(bn + " ");
+            }
+        }
     }
 }
